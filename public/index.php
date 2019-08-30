7cea1d0bef40eb4360813fe22c11549b09729030
Evgeny <xonix75@mail.ru>
2019-08-30 22:54:42 +0300

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+use Framework\Http\Response;
+use Framework\Http\RequestFactory;
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+$request = RequestFactory::fromGlobals();
+$name = $request->getQueryParams()['name'] ?? 'Guest';
+
+$response = (new Response('Hello ' . $name . '!'))
+    ->withHeader('X-Developer', 'Evgeny');
+
+header('HTTP/1.0 ' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
+foreach ($response->getHeaders() as $name => $value) {
+    header($name. ':' . $value);
+}
+echo $response->getBody();
+
