dfc986f2b433865184ed882be77084847b9e5fcc
Evgeny <xonix75@mail.ru>
2019-09-16 00:05:11 +0300

    Part 2

--- public/index.php
+++ public/index.php
@@ -1,14 +1,16 @@
 <?php
 
+use App\Http\Action\AboutAction;
+use App\Http\Action\Blog\IndexAction;
+use App\Http\Action\Blog\ShowAction;
+use App\Http\Action\HelloAction;
+use Framework\Http\ActionResolver;
 use Framework\Http\Router\Exception\RequestNotMatchedException;
 use Framework\Http\Router\RouteCollection;
 use Framework\Http\Router\Router;
-use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\JsonResponse;
 use Zend\Diactoros\ServerRequestFactory;
 use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
-use Zend\Diactoros\Response\HtmlResponse;
-use function foo\func;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
@@ -16,31 +18,13 @@ require 'vendor/autoload.php';
 # Init routes
 $routes = new RouteCollection;
 
-$routes->get('home', '/', function (ServerRequestInterface $request) {
-    $name = $request->getQueryParams()['name'] ?? 'Guest';
-    return new HtmlResponse('Hello, ' . $name . '!');
-});
-
-$routes->get('about', '/about', function () {
-    return new HtmlResponse('I am a simple site.');
-});
-
-$routes->get('blog', '/blog', function () {
-    return new JsonResponse([
-        ['id' => 1, 'title' => 'The First Post'],
-        ['id' => 2, 'title' => 'The Second Post'],
-    ]);
-});
-
-$routes->get('blog_show', '/blog/{id}', function (ServerRequestInterface $request) {
-    $id = $request->getAttribute('id');
-    if ($id > 2) {
-        return new JsonResponse(['error' => 'Undefined page'], 404);
-    }
-    return new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
-}, ['id' => '\d+']);
+$routes->get('home', '/', HelloAction::class);
+$routes->get('about', '/about', AboutAction::class);
+$routes->get('blog', '/blog', IndexAction::class);
+$routes->get('blog_show', '/blog{id}', ShowAction::class, ['id' => '\d+']);
 
 $router = new Router($routes);
+$resolver = new ActionResolver;
 
 # Run
 $request = ServerRequestFactory::fromGlobals();
@@ -50,8 +34,7 @@ try {
     foreach ($result->getAttributes() as $attribute => $value) {
         $request = $request->withAttribute($attribute, $value);
     }
-    /** @var callable $action */
-    $action = $result->getHandler();
+    $action = $resolver->resolve($result->getHandler());
     $response = $action($request);
 } catch (RequestNotMatchedException $e) {
     $response = new JsonResponse(['error' => 'Undefined page'], 404);
