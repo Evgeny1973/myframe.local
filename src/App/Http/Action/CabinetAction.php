effe204b948777856097bfa87b53bf667db07a81
Evgeny <xonix75@mail.ru>
2019-09-21 22:10:27 +0300

    BasicAuthMiddleware

--- src/App/Http/Action/CabinetAction.php
+++ src/App/Http/Action/CabinetAction.php
@@ -2,15 +2,15 @@
 
 namespace App\Http\Action;
 
+use App\Http\Middleware\BasicAuthActionMiddleware;
 use Psr\Http\Message\ServerRequestInterface;
-use Zend\Diactoros\Response\EmptyResponse;
 use Zend\Diactoros\Response\HtmlResponse;
 
 class CabinetAction
 {
     public function __invoke(ServerRequestInterface $request)
     {
-        $username = $request->getAttribute('username');
+        $username = $request->getAttribute(BasicAuthActionMiddleware::ATTRIBUTE);
         return new HtmlResponse('Я залогинен как ' . $username);
     }
 }
--- src/App/Http/Middleware/BasicAuthActionMiddleware.php
+++ src/App/Http/Middleware/BasicAuthActionMiddleware.php
@@ -1,35 +1,31 @@
 <?php
 
-namespace App\Http\Action;
+namespace App\Http\Middleware;
 
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\EmptyResponse;
 
-class BasicAuthActionDecorator
+class BasicAuthActionMiddleware
 {
-    /**
-     * @var callable
-     */
-    protected $next;
     /**
      * @var array
      */
     protected $users;
+    public const ATTRIBUTE = '_user';
 
-    public function __construct(callable $next, array $users)
+    public function __construct(array $users)
     {
-        $this->next = $next;
         $this->users = $users;
     }
 
-    public function __invoke(ServerRequestInterface $request)
+    public function __invoke(ServerRequestInterface $request, callable $next)
     {
         $username = $request->getServerParams()['PHP_AUTH_USER'] ?? null;
         $password = $request->getServerParams()['PHP_AUTH_PW'] ?? null;
         if (!empty($username) && !empty($password)) {
             foreach ($this->users as $name => $pass) {
                 if ($name === $username && $pass === $password) {
-                    return ($this->next)($request->withAttribute('username', $username));
+                    return $next($request->withAttribute(self::ATTRIBUTE, $username));
                 }
             }
         }
